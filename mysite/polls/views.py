104c0642e38b332ad21fbbbaa20b77c3760ebe98
Ricardo Lelis <ricardo.lelis3@gmail.com>
2022-03-19 19:06:17 +0000

    Use try/catch to get data from db and raise 404 case not found.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404
 
 from .models import Question
 
@@ -11,8 +11,11 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    response = f"You are looking at question {question_id}"
-    return HttpResponse(response)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = f"You are looking results of question {question_id}"
